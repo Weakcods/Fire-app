3454e9e0b836189493a089e4a28ae8a1f63f6644
Weakcods <bacayjhoshuajm@gmail.com>
2025-04-10 11:15:35 +0800

    fix error

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth,MultilineIncidentTop3Country,multipleBarbySeverity
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity, map_station
 from fire import views
 
 urlpatterns = [
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
-     path('lineChart/', LineCountbyMonth, name='chart'),
-     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
-      path('multiBarChart/', multipleBarbySeverity, name='chart'),
-
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
+    path('map-station/', map_station, name='map_station'),
 ]
